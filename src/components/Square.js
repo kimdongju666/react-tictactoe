8999cf7dcd4c9c053fd1ae048e15c08b67fd7bb8
kimdongju666@gmail.com <kimdongju666@naver.com>
2022-12-09 23:24:11 +0900

    Functinal component로 바꿈

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Square.css"
 
-export default class Square extends React.Component {
+const Square = ({ onClick, value }) => {
 
-
-  render() {
     return (
       <button className="square" 
-        onClick={() => { this.props.onClick() }}>
-        {this.props.value}
+        onClick={onClick}>
+        {props.value}
       </button>
     )
-  }
 }
+export default Square;
